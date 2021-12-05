ce2eeeb426400ed7007beda7d2191ed1560cd855
johanna-almqvist <94167705+johanna-almqvist@users.noreply.github.com>
2021-12-05 12:43:06 +0100

    fixat issues

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Hämta element first page
 const firstPage = document.getElementById("firstPage");
 const userName = document.getElementById("userName");
@@ -8,35 +7,36 @@ const logginBtn = document.getElementById("logginBtn");
 // Element second page
 const secondPage = document.getElementById("secondPage");
 const loggutBtn = document.getElementById("loggutBtn");
-
 const error = document.getElementById("error");
+
+// Spara användare
+const a = localStorage.getItem("username");
+if (a === "janne") {
+  firstPage.hidden = true;
+  secondPage.hidden = false;
+  error.hidden = true;
+}
 
 // FirstPage
 function correctLoggin() {
-    console.log("klick på knapp");
-  if (userName.value === "Janne" && password.value === "guest") {
+  console.log("klick på knapp");
+  if (userName.value === "janne" && password.value === "test") {
     firstPage.hidden = true;
     secondPage.hidden = false;
     error.hidden = true;
+    localStorage.setItem("username", userName.value);
   } else {
     firstPage.hidden = false;
     secondPage.hidden = true;
     error.hidden = false;
-    alert("Fel lösenord")
+    alert("Fel lösenord");
   }
 }
-function logout(){
-    firstPage.hidden = false;
-    secondPage.hidden = true;
+function logout() {
+  firstPage.hidden = false;
+  secondPage.hidden = true;
+  localStorage.setItem("username", "");
 }
 // Knappar
 logginBtn.addEventListener("click", correctLoggin);
-
 loggutBtn.addEventListener("click", logout);
-
-
-
-
-// SecondPage
-
-// Felmeddelande
